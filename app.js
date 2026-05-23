90c3753cc930ef2bdc16488c33a75e33609d57fe
wzjy5m6z49-droid <wzjy5m6z49@privaterelay.appleid.com>
2026-05-24 07:40:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,68 +1,8 @@
-const app = document.getElementById('tiles');
-
-(window.imageTilesData || []).forEach((item, index) => {
-
-  const image = item.Image || '';
-  const hoverImage = item.HoverImage || '';
-  const link = item.Link || '#';
-
-  const el = document.createElement('a');
-
-  el.className = 'tile hoverEnabled hover_zoom animInit anim_slide';
-
-  el.href = link;
-  el.target = '_blank';
-
-  el.style.setProperty('--i', index);
-
-  el.innerHTML = `
-    <div class="imageWrapper">
-
-      <img
-        class="image imageNormal contain"
-        src="${image}"
-      >
-
-      ${
-        hoverImage
-          ? `
-          <img
-            class="image imageHover contain"
-            src="${hoverImage}"
-          >
-        `
-          : ''
-      }
-
-    </div>
-  `;
-
-  app.appendChild(el);
-
-});
-
-/* ===== スクロールアニメーション ===== */
-
-const observer = new IntersectionObserver(
-  (entries) => {
-
-    entries.forEach((entry) => {
-
-      if (entry.isIntersecting) {
-        entry.target.classList.add('inView');
-      } else {
-        entry.target.classList.remove('inView');
-      }
-
-    });
-
-  },
-  {
-    threshold: 0.15,
-    rootMargin: '0px 0px -10% 0px'
-  }
-);
-
-document.querySelectorAll('.tile').forEach((el) => {
-  observer.observe(el);
-});
+fetch("https://digitalgojp.sharepoint.com/sites/NTA_IBHub12/_api/web/lists/getbytitle('ImageTiles')/items")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
