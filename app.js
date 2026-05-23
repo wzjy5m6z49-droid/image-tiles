004bbbee842b403ddd6607c57b1bb014ce48999d
wzjy5m6z49-droid <wzjy5m6z49@privaterelay.appleid.com>
2026-05-24 07:50:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-document.body.innerHTML = 'TEST START';
-
-fetch("https://digitalgojp.sharepoint.com/sites/NTA_IBHub12/_api/web/lists/getbytitle('ImageTiles')/items")
+fetch("https://digitalgojp.sharepoint.com/sites/NTA_IBHub12/_api/web/lists(guid'd810a0c4-002d-4702-84dc-96366a6480e5')/items")
   .then(response => response.json())
   .then(data => {
 
