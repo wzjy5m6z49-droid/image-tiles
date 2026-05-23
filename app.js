71a85e89166a4e5dfddb4cb8630f154c0e846e7b
wzjy5m6z49-droid <wzjy5m6z49@privaterelay.appleid.com>
2026-05-24 07:42:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+document.body.innerHTML = 'TEST START';
+
 fetch("https://digitalgojp.sharepoint.com/sites/NTA_IBHub12/_api/web/lists/getbytitle('ImageTiles')/items")
   .then(response => response.json())
   .then(data => {
+
     console.log(data);
+
+    document.body.innerHTML =
+      '<pre>' +
+      JSON.stringify(data, null, 2) +
+      '</pre>';
+
   })
   .catch(error => {
+
     console.error(error);
+
+    document.body.innerHTML =
+      '<pre>' +
+      error +
+      '</pre>';
+
   });
